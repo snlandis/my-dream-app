7181cabbd5bb62d8fd1d7688a0cc62dcfd56ba57
Stephen Landis <stephen.n.landis@gmail.com>
2017-07-26 15:23:37 -0500

    have custom component working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; //this was added second
+import { FormsModule } from '@angular/forms'; //(1)this was added second
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component'; //(2)address of the component
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent //(2)tells angular to find the component because it doesn't auto detect
   ],
   imports: [
     BrowserModule,
-    FormsModule //this was added first
+    FormsModule //(1)this was added first
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core'; //is needed to make line 3 work
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
